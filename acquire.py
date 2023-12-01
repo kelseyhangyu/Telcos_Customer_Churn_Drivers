5b643ffa0afe1cadcc8c2539a4984b66f8f208a3
Kelsey H Yu <hangkelseyyu@gmail.com>
2023-12-01 16:34:32 -0600

    fixed functions

--- acquire.py
+++ acquire.py
@@ -1,84 +1,36 @@
 import env
 
-def get_titanic_data():
-
-    import env
-    import os
-    import pandas as pd
-    """
-    takes no arguements, acquires my titanic data from either a csv or sql 
-    returns titanic dataframe
-    """
-    filename = 'titanic.csv'
-    
-    if os.path.exists(filename):
-        print('this file exists, reading from csv')
-        #read from csv
-        df = pd.read_csv(filename, index_col=0) #wont make the index a new column
-    else:
-        print('this file doesnt exist, reading from sql and saving to csv')
-        #read from sql
-        df = pd.read_sql('select * from passengers', url)
-
-        #save to csv
-        df.to_csv(filename)
-        
-    return df 
-
-
-import os
-def get_iris_data():
-
-    import env
-    import os
-    import pandas as pd
-    filename = 'iris.csv'
-    
-    if os.path.exists(filename):
-        print('this file exists, reading from csv')
-        #read from csv
-        df = pd.read_csv(filename, index_col=0) #wont make the index a new column
-    else:
-        print('this file doesnt exist, reading from sql and saving to csv')
-        #read from sql
-        df = pd.read_sql('select * from measurements join species using(species_id)', url)
-
-        #save to csv
-        df.to_csv(filename)
-        
-    return df 
-
 
 def get_telco_data():
     import env
     import os
     import pandas as pd
-    filename = 'telco.csv'
+    filename = 'telco_raw.csv'
     
     if os.path.exists(filename):
         print('this file exists, reading from csv')
         #read from csv
-        df = pd.read_csv(filename, index_col=0) #wont make the index a new column
+        telco = pd.read_csv(filename, index_col=0) #wont make the index a new column
     else:
         print('this file doesnt exist, reading from sql and saving to csv')
         #read from sql
-        df = pd.read_sql(
+        telco = pd.read_sql(
         '''
-SELECT 
-        customers.*,
-        contract_types.contract_type,
-        payment_types.payment_type,
-        internet_service_types.internet_service_type
-    FROM customers
-    LEFT JOIN contract_types ON customers.contract_type_id = contract_types.contract_type_id
-    LEFT JOIN payment_types ON customers.payment_type_id = payment_types.payment_type_id
-    LEFT JOIN internet_service_types ON customers.internet_service_type_id = internet_service_types.internet_service_type_id;
-
+SELECT *
+FROM customers
+LEFT JOIN contract_types ON customers.contract_type_id = contract_types.contract_type_id
+LEFT JOIN customer_churn ON customers.customer_id = customer_churn.customer_id
+LEFT JOIN customer_contracts ON customers.customer_id = customer_contracts.customer_id
+LEFT JOIN customer_details ON customers.customer_id = customer_details.customer_id
+LEFT JOIN customer_payments ON customers.customer_id = customer_payments.customer_id
+LEFT JOIN customer_signups ON customers.customer_id = customer_signups.customer_id
+LEFT JOIN customer_subscriptions ON customers.customer_id = customer_subscriptions.customer_id
+LEFT JOIN internet_service_types ON customer_subscriptions.internet_service_type_id = internet_service_types.internet_service_type_id
+LEFT JOIN payment_types ON customers.payment_type_id = payment_types.payment_type_id
 '''
-        , url)
+        , env.get_db_url('telco_churn', user=env.user, password=env.password, host=env.host))
 
         #save to csv
-        df.to_csv(filename)
+        telco.to_csv(filename)
         
-    return df 
-
+    return telco
